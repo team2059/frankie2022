d5b605121220e732e68c4f1b38e64a9f31047cd7
Techmj <sundaevlogs@gmail.com>
2022-01-30 23:09:28 -0500

    inital commit

---
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+
+public class DriveTrainSubsystem extends SubsystemBase {
+
+  CANSparkMax leftFrontCANSparkMax = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
+  CANSparkMax leftBackCANSparkMax = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
+  CANSparkMax rightFrontCANSparkMax = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
+  CANSparkMax rightBackCANSparkMax = new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+  RelativeEncoder leftRelativeEncoder = leftFrontCANSparkMax.getEncoder();
+  RelativeEncoder rightRelativeEncoder = rightFrontCANSparkMax.getEncoder();
+
+  MotorControllerGroup leftMotorControllerGroup = new MotorControllerGroup(leftFrontCANSparkMax, leftBackCANSparkMax);
+  MotorControllerGroup rightMotorControllerGroup = new MotorControllerGroup(rightFrontCANSparkMax, rightBackCANSparkMax);
+
+  DifferentialDrive differentialDrive = new DifferentialDrive(leftMotorControllerGroup, rightMotorControllerGroup);
+
+  /** Creates a new DriveTrain. */
+  public DriveTrainSubsystem() {
+    //m_right.setInverted(true);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void arcadeDrive(double xSpeed, double zRotation) {
+    differentialDrive.arcadeDrive(xSpeed, zRotation);
+  }
+}
