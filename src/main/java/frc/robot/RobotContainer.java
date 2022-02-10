17096c7e68ea6301dc48f85bdaa25ac3f8a2bb7c
Techmj <sundaevlogs@gmail.com>
2022-02-09 22:51:58 -0500

    added paths.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -41,130 +41,130 @@ import edu.wpi.first.wpilibj.XboxController;
  */
 public class RobotContainer {
 
-  // The robot's subsystems and commands are defined here...
-  public static XboxController logitech = new XboxController(3);
-  private final DriveTrainSubsystem driveTrainSubsystem = new DriveTrainSubsystem();
+        // The robot's subsystems and commands are defined here...
+        public static XboxController logitech = new XboxController(3);
+        private final DriveTrainSubsystem driveTrainSubsystem = new DriveTrainSubsystem();
 
-  // private final TeleopDriveCommand teleopDriveCommand = new
-  // TeleopDriveCommand(driveTrainSubsystem);
+        // private final TeleopDriveCommand teleopDriveCommand = new
+        // TeleopDriveCommand(driveTrainSubsystem);
 
-  Command autonomousCommand;
+        Command autonomousCommand;
 
-  /**
-   * The container for the robot. Contains subsystems, OI devices, and commands.
-   */
-  public RobotContainer() {
-    // Configure the button bindings
-    configureButtonBindings();
-    driveTrainSubsystem.setDefaultCommand(new DriveCmd(driveTrainSubsystem));
-  }
+        /**
+         * The container for the robot. Contains subsystems, OI devices, and commands.
+         */
+        public RobotContainer() {
+                // Configure the button bindings
+                configureButtonBindings();
+                driveTrainSubsystem.setDefaultCommand(new DriveCmd(driveTrainSubsystem));
+        }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be
-   * created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
-   * it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {
+        /**
+         * Use this method to define your button->command mappings. Buttons can be
+         * created by
+         * instantiating a {@link GenericHID} or one of its subclasses ({@link
+         * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+         * it to a {@link
+         * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+         */
+        private void configureButtonBindings() {
 
-  }
+        }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   * @throws IOException
-   */
+        /**
+         * Use this to pass the autonomous command to the main {@link Robot} class.
+         *
+         * @return the command to run in autonomous
+         * @throws IOException
+         */
 
-  public Command getAutonomousCommand() throws IOException {
-    driveTrainSubsystem.zeroHeading();
-    driveTrainSubsystem.resetEncoders();
-    // set left to inverted so motors go in same direction
-    // driveTrainSubsystem.getLeftMotorControllerGroup().setInverted(true);
-    // driveTrainSubsystem.resetEncoders();
-    // driveTrainSubsystem.zeroHeading();
+        public Command getAutonomousCommand() throws IOException {
+                driveTrainSubsystem.zeroHeading();
+                driveTrainSubsystem.resetEncoders();
+                // set left to inverted so motors go in same direction
+                // driveTrainSubsystem.getLeftMotorControllerGroup().setInverted(true);
+                // driveTrainSubsystem.resetEncoders();
+                // driveTrainSubsystem.zeroHeading();
 
-    // Create a voltage constraint to ensure we don't accelerate too fast
-    var autoVoltageConstraint = new DifferentialDriveVoltageConstraint(
-        new SimpleMotorFeedforward(
-            DriveConstants.ksVolts,
-            DriveConstants.kvVoltSecondsPerMeter,
-            DriveConstants.kaVoltSecondsSquaredPerMeter),
-        DriveConstants.kDriveKinematics,
-        10);
+                // Create a voltage constraint to ensure we don't accelerate too fast
+                var autoVoltageConstraint = new DifferentialDriveVoltageConstraint(
+                                new SimpleMotorFeedforward(
+                                                DriveConstants.ksVolts,
+                                                DriveConstants.kvVoltSecondsPerMeter,
+                                                DriveConstants.kaVoltSecondsSquaredPerMeter),
+                                DriveConstants.kDriveKinematics,
+                                10);
 
-    // Create config for trajectory
-    TrajectoryConfig config = new TrajectoryConfig(
-        AutoConstants.kMaxSpeedMetersPerSecond,
-        AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-            // Add kinematics to ensure max speed is actually obeyed
-            .setKinematics(DriveConstants.kDriveKinematics)
-            // Apply the voltage constraint
-            .addConstraint(autoVoltageConstraint);
+                // Create config for trajectory
+                TrajectoryConfig config = new TrajectoryConfig(
+                                AutoConstants.kMaxSpeedMetersPerSecond,
+                                AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+                                                // Add kinematics to ensure max speed is actually obeyed
+                                                .setKinematics(DriveConstants.kDriveKinematics)
+                                                // Apply the voltage constraint
+                                                .addConstraint(autoVoltageConstraint);
 
-    // An example trajectory to follow. All units in meters.
-    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-        // Start at the origin facing the +X direction
-        new Pose2d(0, 0, new Rotation2d(0)),
-        // Pass through these two interior waypoints, making an 's' curve path
-        List.of(new Translation2d(1, 0), new Translation2d(1.5, 0)),
-        // End 3 meters straight ahead of where we started, facing forward
-        new Pose2d(2, 0, new Rotation2d(0)),
-        // Pass config
-        config);
+                // An example trajectory to follow. All units in meters.
+                Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+                                // Start at the origin facing the +X direction
+                                new Pose2d(0, 0, new Rotation2d(0)),
+                                // Pass through these two interior waypoints, making an 's' curve path
+                                List.of(new Translation2d(1, 0), new Translation2d(1.5, 0)),
+                                // End 3 meters straight ahead of where we started, facing forward
+                                new Pose2d(2, 0, new Rotation2d(0)),
+                                // Pass config
+                                config);
 
-    Path trajectoryPath = Filesystem.getDeployDirectory().toPath()
-        .resolve("pathplanner/generatedJSON/Forward2.wpilib.json");
-    Trajectory trajectory = new Trajectory();
-    trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+                Path trajectoryPath = Filesystem.getDeployDirectory().toPath()
+                                .resolve("pathplanner/generatedJSON/Forward2.wpilib.json");
+                Trajectory trajectory = new Trajectory();
+                trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
 
-    // driveTrainSubsystem.resetOdometry(trajectory.getInitialPose());
-    // driveTrainSubsystem.getField().getObject("Pose").setTrajectory(trajectory);
-    // Rotation2d rotation2d =
-    // driveTrainSubsystem.getPose().minus(trajectory.getInitialPose());
+                // driveTrainSubsystem.resetOdometry(trajectory.getInitialPose());
+                // driveTrainSubsystem.getField().getObject("Pose").setTrajectory(trajectory);
+                // Rotation2d rotation2d =
+                // driveTrainSubsystem.getPose().minus(trajectory.getInitialPose());
 
-    // Transform2d rotate90 = new Pose2d(0, 0, Rotation2d.fromDegrees(90));
-    // trajectory = trajectory.transformBy(new Transform2d(new Translation2d(0.0,
-    // 0.0), Rotation2d.fromDegrees(110)));
+                // Transform2d rotate90 = new Pose2d(0, 0, Rotation2d.fromDegrees(90));
+                // trajectory = trajectory.transformBy(new Transform2d(new Translation2d(0.0,
+                // 0.0), Rotation2d.fromDegrees(110)));
 
-    // driveTrainSubsystem.resetOdometry(trajectory.getInitialPose());
+                // driveTrainSubsystem.resetOdometry(trajectory.getInitialPose());
 
-    // driveTrainSubsystem.differentialDrive.resetOdometry(trajectory.getInitialPose());
-    // transforming the PathWeaver trajectory to match your current pose
-    // var transform =
-    // driveTrainSubsystem.getPose().minus(trajectory.getInitialPose());
-    // straightTrajectory = trajectory.transformBy(transform);
+                // driveTrainSubsystem.differentialDrive.resetOdometry(trajectory.getInitialPose());
+                // transforming the PathWeaver trajectory to match your current pose
+                // var transform =
+                // driveTrainSubsystem.getPose().minus(trajectory.getInitialPose());
+                // straightTrajectory = trajectory.transformBy(transform);
 
-    RamseteCommand ramseteCommand = new RamseteCommand(
-        trajectory,
-        driveTrainSubsystem::getPose,
-        new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
-        new SimpleMotorFeedforward(
-            DriveConstants.ksVolts,
-            DriveConstants.kvVoltSecondsPerMeter,
-            DriveConstants.kaVoltSecondsSquaredPerMeter),
-        DriveConstants.kDriveKinematics,
-        driveTrainSubsystem::getWheelSpeeds,
-        new PIDController(DriveConstants.kPDriveVel, 0, 0),
-        new PIDController(DriveConstants.kPDriveVel, 0, 0),
-        // RamseteCommand passes volts to the callback
-        driveTrainSubsystem::tankDriveVolts,
-        driveTrainSubsystem);
+                RamseteCommand ramseteCommand = new RamseteCommand(
+                                trajectory,
+                                driveTrainSubsystem::getPose,
+                                new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
+                                new SimpleMotorFeedforward(
+                                                DriveConstants.ksVolts,
+                                                DriveConstants.kvVoltSecondsPerMeter,
+                                                DriveConstants.kaVoltSecondsSquaredPerMeter),
+                                DriveConstants.kDriveKinematics,
+                                driveTrainSubsystem::getWheelSpeeds,
+                                new PIDController(DriveConstants.kPDriveVel, 0, 0),
+                                new PIDController(DriveConstants.kPDriveVel, 0, 0),
+                                // RamseteCommand passes volts to the callback
+                                driveTrainSubsystem::tankDriveVolts,
+                                driveTrainSubsystem);
 
-    // Reset odometry to the starting pose of the trajectory.
-    // driveTrainSubsystem.resetOdometry(trajectory.getInitialPose());
-    driveTrainSubsystem.resetOdometry(trajectory.getInitialPose());
-    // Run path following command, then stop at the end via break mode to ensure no
-    // voltage and then set motors to idle mode for teleOp.
-    return ramseteCommand.andThen(() -> driveTrainSubsystem.setBreakMode())
-        .andThen(() -> driveTrainSubsystem.setIdleMode());
+                // Reset odometry to the starting pose of the trajectory.
+                // driveTrainSubsystem.resetOdometry(trajectory.getInitialPose());
+                driveTrainSubsystem.resetOdometry(trajectory.getInitialPose());
+                // Run path following command, then stop at the end via break mode to ensure no
+                // voltage and then set motors to idle mode for teleOp.
+                return ramseteCommand.andThen(() -> driveTrainSubsystem.setBreakMode())
+                                .andThen(() -> driveTrainSubsystem.setIdleMode());
 
-  }
+        }
 
-  public DriveTrainSubsystem getDriveTrainSubsystem() {
-    return driveTrainSubsystem;
-  }
+        public DriveTrainSubsystem getDriveTrainSubsystem() {
+                return driveTrainSubsystem;
+        }
 
 }
