0215e3739f7e995f8c3e8b52e2294dace327058f
Techmj <mrenaaljain@gmail.com>
2022-02-09 22:49:58 -0500

    final turning paths working. negated left side.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,59 +4,101 @@
 
 package frc.robot;
 
+import java.io.IOException;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.DriveTrainSubsystem;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
 
+  private Command m_autonomousCommand;
   private RobotContainer m_robotContainer;
+  // private DriveTrainSubsystem driveTrainSubsystem;
 
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+
+    // Instantiate our RobotContainer. This will perform all our button bindings,
+    // and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and
+   * test.
    *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and
    * SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // Runs the Scheduler. This is responsible for polling buttons, adding
+    // newly-scheduled
+    // commands, running already-scheduled commands, removing finished or
+    // interrupted commands,
+    // and running subsystem periodic() methods. This must be called from the
+    // robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+    m_robotContainer.getDriveTrainSubsystem().zeroHeading();
+    m_robotContainer.getDriveTrainSubsystem().resetEncoders();
+
+    // m_robotContainer.getDriveTrainSubsystem().zeroHeading();
+    // m_robotContainer.getDriveTrainSubsystem().resetEncoders();
+
+  }
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  /**
+   * This autonomous runs the autonomous command selected by your
+   * {@link RobotContainer} class.
+   */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    m_robotContainer.getDriveTrainSubsystem().zeroHeading();
+    m_robotContainer.getDriveTrainSubsystem().resetEncoders();
+
+    try {
+      m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
@@ -66,10 +108,53 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+
+    // if (driveTrainSubsystem.getHeading() != 0) {
+    // driveTrainSubsystem.zeroHeading();
+    // }
+
+  }
 
   @Override
   public void teleopInit() {
+    m_robotContainer.getDriveTrainSubsystem().zeroHeading();
+    m_robotContainer.getDriveTrainSubsystem().resetEncoders();
+
+    Rotation2d thetaPose = m_robotContainer.getDriveTrainSubsystem().navX.getRotation2d();
+    DifferentialDriveOdometry myDriveOdometry = m_robotContainer.getDriveTrainSubsystem().getOdometry();
+    myDriveOdometry.resetPosition(new Pose2d(), thetaPose);
+
+    // if(null == driveTrainSubsystem) {
+    // SmartDashboard.putString("driveTrainSubsystem" , "is NULLL");
+    // }
+    // driveTrainSubsystem.zeroHeading();
+    // driveTrainSubsystem.resetEncoders();
+
+    // Rotation2d thetaPose = driveTrainSubsystem.navX.getRotation2d();
+
+    // DifferentialDriveOdometry myDriveOdometry = null;
+    // try {
+    // myDriveOdometry = driveTrainSubsystem.getOdometry();
+    // }catch (NullPointerException ex){
+    // SmartDashboard.putString("myDriveOdometry" , "try catch NULLL");
+    // ex.printStackTrace();
+
+    // }
+
+    // if(null == myDriveOdometry) {
+    // SmartDashboard.putString("myDriveOdometry" , "is NULLL");
+    // }
+
+    // try{
+    // myDriveOdometry.resetPosition(new Pose2d(), thetaPose);
+    // }catch(NullPointerException exception){
+    // SmartDashboard.putString("myDriveOdometry" , "cannot reset pos");
+    // }
+    //
+
+    // driveTrainSubsystem.getOdometry().resetPosition(new Pose2d(), thetaPose);
+
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
@@ -81,7 +166,8 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+  }
 
   @Override
   public void testInit() {
@@ -91,5 +177,6 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,52 +4,167 @@
 
 package frc.robot;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.commands.TeleopDriveCommand;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.commands.DriveCmd;
 import frc.robot.subsystems.DriveTrainSubsystem;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  
+
   // The robot's subsystems and commands are defined here...
-  public static Joystick joyStick = new Joystick(0);
-  
+  public static XboxController logitech = new XboxController(3);
   private final DriveTrainSubsystem driveTrainSubsystem = new DriveTrainSubsystem();
 
-  //private final TeleopDriveCommand teleopDriveCommand = new TeleopDriveCommand(driveTrainSubsystem);
+  // private final TeleopDriveCommand teleopDriveCommand = new
+  // TeleopDriveCommand(driveTrainSubsystem);
 
   Command autonomousCommand;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
-
-    driveTrainSubsystem.setDefaultCommand(new TeleopDriveCommand(driveTrainSubsystem));
+    driveTrainSubsystem.setDefaultCommand(new DriveCmd(driveTrainSubsystem));
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+   * it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
+   * @throws IOException
    */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return autonomousCommand;
+
+  public Command getAutonomousCommand() throws IOException {
+    driveTrainSubsystem.zeroHeading();
+    driveTrainSubsystem.resetEncoders();
+    // set left to inverted so motors go in same direction
+    // driveTrainSubsystem.getLeftMotorControllerGroup().setInverted(true);
+    // driveTrainSubsystem.resetEncoders();
+    // driveTrainSubsystem.zeroHeading();
+
+    // Create a voltage constraint to ensure we don't accelerate too fast
+    var autoVoltageConstraint = new DifferentialDriveVoltageConstraint(
+        new SimpleMotorFeedforward(
+            DriveConstants.ksVolts,
+            DriveConstants.kvVoltSecondsPerMeter,
+            DriveConstants.kaVoltSecondsSquaredPerMeter),
+        DriveConstants.kDriveKinematics,
+        10);
+
+    // Create config for trajectory
+    TrajectoryConfig config = new TrajectoryConfig(
+        AutoConstants.kMaxSpeedMetersPerSecond,
+        AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+            // Add kinematics to ensure max speed is actually obeyed
+            .setKinematics(DriveConstants.kDriveKinematics)
+            // Apply the voltage constraint
+            .addConstraint(autoVoltageConstraint);
+
+    // An example trajectory to follow. All units in meters.
+    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+        // Start at the origin facing the +X direction
+        new Pose2d(0, 0, new Rotation2d(0)),
+        // Pass through these two interior waypoints, making an 's' curve path
+        List.of(new Translation2d(1, 0), new Translation2d(1.5, 0)),
+        // End 3 meters straight ahead of where we started, facing forward
+        new Pose2d(2, 0, new Rotation2d(0)),
+        // Pass config
+        config);
+
+    Path trajectoryPath = Filesystem.getDeployDirectory().toPath()
+        .resolve("pathplanner/generatedJSON/Forward2.wpilib.json");
+    Trajectory trajectory = new Trajectory();
+    trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+
+    // driveTrainSubsystem.resetOdometry(trajectory.getInitialPose());
+    // driveTrainSubsystem.getField().getObject("Pose").setTrajectory(trajectory);
+    // Rotation2d rotation2d =
+    // driveTrainSubsystem.getPose().minus(trajectory.getInitialPose());
+
+    // Transform2d rotate90 = new Pose2d(0, 0, Rotation2d.fromDegrees(90));
+    // trajectory = trajectory.transformBy(new Transform2d(new Translation2d(0.0,
+    // 0.0), Rotation2d.fromDegrees(110)));
+
+    // driveTrainSubsystem.resetOdometry(trajectory.getInitialPose());
+
+    // driveTrainSubsystem.differentialDrive.resetOdometry(trajectory.getInitialPose());
+    // transforming the PathWeaver trajectory to match your current pose
+    // var transform =
+    // driveTrainSubsystem.getPose().minus(trajectory.getInitialPose());
+    // straightTrajectory = trajectory.transformBy(transform);
+
+    RamseteCommand ramseteCommand = new RamseteCommand(
+        trajectory,
+        driveTrainSubsystem::getPose,
+        new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
+        new SimpleMotorFeedforward(
+            DriveConstants.ksVolts,
+            DriveConstants.kvVoltSecondsPerMeter,
+            DriveConstants.kaVoltSecondsSquaredPerMeter),
+        DriveConstants.kDriveKinematics,
+        driveTrainSubsystem::getWheelSpeeds,
+        new PIDController(DriveConstants.kPDriveVel, 0, 0),
+        new PIDController(DriveConstants.kPDriveVel, 0, 0),
+        // RamseteCommand passes volts to the callback
+        driveTrainSubsystem::tankDriveVolts,
+        driveTrainSubsystem);
+
+    // Reset odometry to the starting pose of the trajectory.
+    // driveTrainSubsystem.resetOdometry(trajectory.getInitialPose());
+    driveTrainSubsystem.resetOdometry(trajectory.getInitialPose());
+    // Run path following command, then stop at the end via break mode to ensure no
+    // voltage and then set motors to idle mode for teleOp.
+    return ramseteCommand.andThen(() -> driveTrainSubsystem.setBreakMode())
+        .andThen(() -> driveTrainSubsystem.setIdleMode());
+
   }
+
+  public DriveTrainSubsystem getDriveTrainSubsystem() {
+    return driveTrainSubsystem;
+  }
+
 }
